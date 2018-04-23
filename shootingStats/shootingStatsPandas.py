cef1906d604d4b0f9615fed89bb9445c79de3d72
oortj <j.van.oort@umcg.nl>
2018-04-23 12:05:51 +0200

    Courses toegevoegd

--- shootingStats/shootingStatsPandas.py
+++ shootingStats/shootingStatsPandas.py
@@ -4,6 +4,8 @@ from datetime import date, datetime
 
 df = pd.read_csv('TargetScanData.csv', index_col=0, parse_dates=['Event Date'])
 datum = date(int('2018'),int('01'),int('01'))
+
+print(datum)
 currentYear = df[(df['Event Date'] > datum) & (df['Mean Radius'] < 20)]
 
 print(currentYear)
