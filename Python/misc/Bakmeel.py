9b5ddb7ee72aa584b748b72243232ba415338cdd
oortj <j.van.oort@umcg.nl>
2018-05-09 11:30:20 +0200

    commit

--- Python/misc/Bakmeel.py
+++ Python/misc/Bakmeel.py
@@ -1,6 +1,13 @@
-import sys
+import argparse
 
 if __name__ == "__main__":
-    naam = input('Wat is je naam?\n')
-    print('Hallo {}'.format(naam))
-    # print('hallo {}'.format(sys.argv[1]))
+
+    input_formule = input('Vul de bakkerformule in als volgt: volkorenmeel:70, bloem:30, etc.\n')
+    dict_formule = dict(x.split(':') for x in input_formule.split(','))
+
+    input_hoeveelheid = input ('Voer de gewenste hoeveelheid meel in (in grammen):\n')
+    
+    for key in dict_formule:
+        print(key, ':', (float(dict_formule[key]) * int(input_hoeveelheid))/100, 'gr')
+
+    # print(dict_formule)
