433a9c85aa2eca641f5db4542806dde3da54473e
oortj <j.van.oort@umcg.nl>
2018-05-09 12:05:09 +0200

    wijziging in text

--- Python/misc/Bakmeel.py
+++ Python/misc/Bakmeel.py
@@ -2,12 +2,12 @@ import argparse
 
 if __name__ == "__main__":
 
-    input_formule = input('Vul de bakkerformule in als volgt: volkorenmeel:70, bloem:30, etc.\n')
+    input_formule = input('Vul de bakkerformule in (volkorenmeel:70, bloem:30, etc.):\n')
     dict_formule = dict(x.split(':') for x in input_formule.split(','))
 
-    input_hoeveelheid = input ('Voer de gewenste hoeveelheid meel in (in grammen):\n')
+    input_hoeveelheid = input ('Voer de gewenste maxinale hoeveelheid meel in (in grammen): ')
     
     for key in dict_formule:
-        print(key, ':', (float(dict_formule[key]) * int(input_hoeveelheid))/100, 'gr')
+        print(key, ':', (float(dict_formule[key]) * int(input_hoeveelheid))/100, 'gr.')
 
     # print(dict_formule)
