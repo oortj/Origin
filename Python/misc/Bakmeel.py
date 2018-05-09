a54701a3d0f13897d693d842c6ee49e410431bf2
oortj <j.van.oort@umcg.nl>
2018-05-09 12:18:31 +0200

    typfoutje weggewerkt

--- Python/misc/Bakmeel.py
+++ Python/misc/Bakmeel.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     input_formule = input('Vul de bakkerformule in (volkorenmeel:70, bloem:30, etc.):\n')
     dict_formule = dict(x.split(':') for x in input_formule.split(','))
 
-    input_hoeveelheid = input ('Voer de gewenste maxinale hoeveelheid meel in (in grammen): ')
+    input_hoeveelheid = input ('Voer de gewenste maximale hoeveelheid meel in (in grammen): ')
     
     for key in dict_formule:
         print(key, ':', (float(dict_formule[key]) * int(input_hoeveelheid))/100, 'gr.')
